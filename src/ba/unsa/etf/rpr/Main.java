b29edf8e9d8f32f55dc4273be4fea0540b8e207e
lbuturovic <lbuturovic1@etf.unsa.ba>
2019-10-14 14:28:16 +0200

    initial commit

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,27 +1,16 @@
 package ba.unsa.etf.rpr;
-import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	Scanner S= new Scanner(System.in);
-	System.out.printf("Unesite broj n: ");
-	int n;
-	n=S.nextInt();
-	for(int i=1; i<=n; i++){
-	   // int suma=0;
-	    if(i%sumaCifara(i)==0) System.out.println(i);
-    }
-
-    }
-
-    private static int sumaCifara(int i) {
-        int suma=0;
-        do {
-            suma+=i%10;
-            i=i/10;
-
-        }while(i!=0);
-        return suma;
+        Sat s=new Sat(15,30,45);
+        s.ispisi();
+        s.sljedeci();
+        s.ispisi();
+        s.pomjeriZa(-48);
+        s.ispisi();
+        s.postavi(0,0,0);
+        s.ispisi();
 
     }
 }
